6cf8cfc379ec8cc62a72868fee4ef6b62d9794d1
codewedo <44037537+codewedo@users.noreply.github.com>
2019-10-02 23:25:24 +0530

    Create graph.cpp

---
+++ graph.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class graph{
+  int V;
+  list<int> * adj;
+  public:
+  graph(int v){
+    this->V = v;
+    adj = new vector<int> adj[v];
+  }
+  addEdge(int v, int u){
+      adj[v].push_back(u);
+  }
+int main(){
+}
